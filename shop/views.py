59fdeeb23df66fce79bcad83003f954df5b50b40
AlexVJack <alexvladjack@gmail.com>
2019-11-01 07:35:20 +0200

    Added customer cart

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
@@ -14,5 +14,5 @@ def product_list(request, category_slug=None):
 					'products': products})
 
 def product_detail(request, id, slug):
-	pruduct = get_object_or_404(Product, id=id, slug=slug, available=True)
+	product = get_object_or_404(Product, id=id, slug=slug, available=True)
 	return render(request, 'shop/product/detail.html', {'product': product})
